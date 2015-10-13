0a0d5d2f5614eab11fa32462fcf9684058c535ee
Alex Osborne <aosborne@nla.gov.au>
2015-10-14 09:22:18 +1100

    Add systemd unit files

--- experimental/logduct/setup.py
+++ experimental/logduct/setup.py
@@ -6,12 +6,15 @@ setup(
     description = "Yet another logging daemon",
     license = 'MIT',
     url = "https://github.com/nla/jvmctl",
-    packages = find_packages(exclude=['build', 'dist']),
+    packages = ["logduct"],
     test_suite = "tests",
     entry_points = {
-      'console_scripts': [
-        'logductd=logduct.daemon:main',
-        'logduct-run=logduct.run:main',
+        'console_scripts': [
+            'logductd=logduct.daemon:main',
+            'logduct-run=logduct.run:main',
       ],
     },
+    data_files = [
+        ("/usr/lib/systemd/system/", ["systemd/logductd.service", "systemd/logductd.socket"]),
+    ],
 )
