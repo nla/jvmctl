37afe46f09b6d0c6987b3e0f6bbe990ff4729f58
Alex Osborne <aosborne@nla.gov.au>
2015-10-13 14:37:00 +1100

    Add setup.py build script

---
+++ experimental/logduct/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "logduct",
+    version = "0.1.0",
+    description = "Yet another logging daemon",
+    license = 'MIT',
+    url = "https://github.com/nla/jvmctl",
+    packages = find_packages(exclude=['build', 'dist']),
+    entry_points = {
+      'console_scripts': [
+        'logductd=logduct.daemon.main',
+        'logduct-run=logduct.run.main',
+      ],
+    },
+)
