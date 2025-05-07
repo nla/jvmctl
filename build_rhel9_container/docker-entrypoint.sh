2fcb7de86689374a520aad9483fe9d62bf8904a9
Peter Hine <phine@nla.gov.au>
2025-05-07 16:45:53 +1000

    fixes to make the running of the container by any user

--- build_rhel9_container/docker-entrypoint.sh
+++ build_rhel9_container/docker-entrypoint.sh
@@ -1,10 +1,12 @@
 #!/bin/bash -x
 # check for rpm_build.sh and run it.
-cd /home/builder/rpmbuild/
+owner=$(stat -c '%U' /tmp/rpmbuild)
+cd /tmp/rpmbuild/
 dir=$(pwd)
 for script in $(ls rpm_build.sh */rpm_build.sh 2>/dev/null);
 do 
   echo "Runinng script: ${script} ..."
   cd "${dir}/$(dirname "${script}")"
-  bash -x ./rpm_build.sh
+  # preserve file ownership so the user who ran this can cleanup the files
+  runuser -u $owner bash ./rpm_build.sh
 done
--- jvmctl/run_rpmbuild8_container.sh
+++ jvmctl/run_rpmbuild8_container.sh
@@ -1,16 +1,33 @@
 #!/bin/bash
+# WARNING: This script is designed to run the container in rootless mode (ie: as the user running the script)
+
 VER=8
 Default='\e[0m';Red='\e[31m';Green='\e[32m';Yellow='\e[33m';Cyan='\e[36m';LBlue='\e[94m'
 script_dir=$(dirname $(readlink -f $0))
 cd "${script_dir}"
 
-/usr/bin/rm -rf /tmp/rpmbuild
-mkdir -p /tmp/rpmbuild
-cp -r * /tmp/rpmbuild/
-/usr/bin/podman run --userns=keep-id -v /tmp/rpmbuild:/home/builder/rpmbuild:Z --replace --name=rpmbuild${VER} localhost/rpmbuild${VER} >/tmp/rpmbuild${VER}.txt 2>&1
-SRC_RPM=$(ls /tmp/rpmbuild/dist.el${VER}/*noarch.rpm 2>/dev/null)
-[ "${SRC_RPM}" ] || { echo -e "${Red}No RPM produced. ${Default}Check /tmp/rpmbuild${VER}.txt"; exit 1; } && { mkdir -p "${script_dir}/dist"; cp "${SRC_RPM}" "${script_dir}/dist"; }
+id=$(/usr/bin/id -u)
+[ "${id}" == "0" ] && { echo -e '$RedDO NOT runn as root'; exit 1; }
+
+
+/usr/bin/rm -rf /tmp/${USER}_rpmbuild
+mkdir -p /tmp/${USER}_rpmbuild
+cp -r * /tmp/${USER}_rpmbuild/
+/usr/bin/podman run --userns=keep-id -v /tmp/${USER}_rpmbuild:/tmp/rpmbuild:Z --replace --name=rpmbuild${VER} localhost/rpmbuild${VER} >/tmp/${USER}_rpmbuild${VER}.txt 2>&1
+SRC_RPM=$(ls /tmp/${USER}_rpmbuild/dist.el${VER}/*noarch.rpm 2>/dev/null)
+[ "${SRC_RPM}" ] || { echo -e "${Red}No RPM produced. ${Default}Check /tmp/${USER}_rpmbuild${VER}.txt"; exit 1; } && { mkdir -p "${script_dir}/dist"; cp "${SRC_RPM}" "${script_dir}/dist"; }
 RPM="$(ls ${script_dir}/dist | grep -v '\.el')"
 DST_RPM="$(echo ${RPM} | sed "s:.noarch:.el${VER}.noarch:")"
 mv "dist/${RPM}" "dist/${DST_RPM}"
-echo "Please sign dist/$(ls ${script_dir}/dist)"
+
+read -p "sign 'dist/${DST_RPM}' now ? (y/n): " answer
+case "$answer" in
+  [yY])
+    rpm --addsign "dist/${DST_RPM}"
+    break
+    ;;
+  *)
+    echo "Please sign dist/${DST_RPM}"
+    ;;
+esac
+
